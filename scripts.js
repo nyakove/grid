c05a5203c3c2a074e3c5459e4666f338a7101e18
nyakove <43004249+nyakove@users.noreply.github.com>
2018-11-15 19:41:28 +0200

    cleanup

--- scripts.js
+++ scripts.js
@@ -3,7 +3,6 @@ observer_test();
 function showImage(event) {
     if (!event.target.src) return false;
     document.getElementById('splash').style.display = 'block';
-    //document.getElementById('splash').innerHTML = event.target.outerHTML;
     document.getElementById('splsh').src = event.target.src;
     //console.log(event.target);
     //console.log(event.target.src);
@@ -14,22 +13,10 @@ function escape(event) {
     document.getElementById('splash').style.display = 'none';
 }
 
-/*
-function animate() {
-    if (document.getElementById('splash').style.display == 'block') {
-      document.getElementById('splash').style.transition = 'all 2s ease 0s'  
-    }
-    else {
-        document.getElementById('splash').style.transition = ''
-    }
-}
-*/
 function observer_test() {
     let target = document.querySelector('#splash');
 
     function callback() {
-        //console.log('Mutation!')
-        //document.getElementById("test").innerHTML = '<div style=\'width: 50%; height: 50%; background-color: green;\'></div>'
         document.getElementById('splash-image').style.display = 'block';
     }
     let observer = new MutationObserver(callback);
@@ -43,4 +30,3 @@ function observer_test() {
 
 document.getElementById('main').addEventListener('click', showImage);
 document.getElementById('close').addEventListener('click', escape);
-//document.getElementById('main').addEventListener('click', animate);
