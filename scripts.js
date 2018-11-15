99e1388d3dfcb3667cc0b0d4ae35aead5fc01440
admin3 <admin3@unity.local>
2018-11-15 13:07:19 +0200

    images splash

--- scripts.js
+++ scripts.js
@@ -1,11 +1,46 @@
-function showImage (event) {
-document.getElementById('splash').style.display = 'block';
-document.getElementById('splash').innerHTML = event.target.outerHTML; 
+observer_test();
+
+function showImage(event) {
+    if (!event.target.src) return false;
+    document.getElementById('splash').style.display = 'block';
+    //document.getElementById('splash').innerHTML = event.target.outerHTML;
+    document.getElementById('splsh').src = event.target.src;
+    //console.log(event.target);
+    //console.log(event.target.src);
+
 }
 
-function escape (event) {
-   document.getElementById('splash').style.display = 'none'; 
+function escape(event) {
+    document.getElementById('splash').style.display = 'none';
+}
+
+/*
+function animate() {
+    if (document.getElementById('splash').style.display == 'block') {
+      document.getElementById('splash').style.transition = 'all 2s ease 0s'  
+    }
+    else {
+        document.getElementById('splash').style.transition = ''
+    }
+}
+*/
+function observer_test() {
+    let target = document.querySelector('#splash');
+
+    function callback(mutationsList, observer) {
+        console.log('Mutation!')
+        //document.getElementById("test").innerHTML = '<div style=\'width: 50%; height: 50%; background-color: green;\'></div>'
+        document.getElementById('splash-image').style.display = 'block';
+    }
+    let observer = new MutationObserver(callback);
+
+    let config = {
+        attributes: true,
+    }
+
+    observer.observe(target, config);
 }
 
 document.getElementById('main').addEventListener('click', showImage);
-document.getElementById('splash').addEventListener('click', escape);
+document.getElementById('close').addEventListener('click', escape);
+//document.getElementById('main').addEventListener('click', animate);
